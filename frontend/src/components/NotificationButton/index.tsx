8f971d66eb31c45570135cd6af9a5307067ea40d
Benior <benior.santos@gmail.com>
2022-07-13 14:59:42 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import icon from '../../assets/img/notifi-ico.svg'
+
+
+function NotificationButton() {
+    return (
+        <>
+            <div className="dsmeta-btn-red">
+                <img src={icon} alt="Notificar"/>
+            </div>
+        </>
+    )
+}
+
+export default NotificationButton
